a7703828c56c29d783c9110167fb453f7015078a
Bryce Mercines <nplixel@gmail.com>
2020-07-18 01:37:44 +0800

    removed codebase, added generated build for test

---
+++ precache-manifest.61ce486053e6cc7fddb4f7b250ba37f4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3afda75aaa07290a0bdb379211750d00",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5af88a3166a6c5931272",
+    "url": "./static/css/main.3c62dd61.chunk.css"
+  },
+  {
+    "revision": "46890bb74e0e8ffa55d8",
+    "url": "./static/js/2.3266cfe1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3266cfe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af88a3166a6c5931272",
+    "url": "./static/js/main.ad860d13.chunk.js"
+  },
+  {
+    "revision": "f69a0ac5bc0e7fe5cae1",
+    "url": "./static/js/runtime-main.8012acb5.js"
+  },
+  {
+    "revision": "f14714b2816268c37093d4a9997b735a",
+    "url": "./static/media/campus1.f14714b2.jpg"
+  },
+  {
+    "revision": "73fd041bbe2a6a743a267009d66708c5",
+    "url": "./static/media/cpf_trans.73fd041b.png"
+  },
+  {
+    "revision": "8126e4423c475a722ef27410b7bbe959",
+    "url": "./static/media/preloader.8126e442.svg"
+  },
+  {
+    "revision": "9c3f89873e01fd350626ae0f348b375a",
+    "url": "./static/media/services-bg.9c3f8987.jpg"
+  },
+  {
+    "revision": "e21af197fb366a515e800566d3896f42",
+    "url": "./static/media/subscribe-bg.e21af197.jpg"
+  }
+]);
